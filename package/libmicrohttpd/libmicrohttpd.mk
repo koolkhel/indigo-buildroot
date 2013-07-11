b189273dde0a5a902e402bdf78bfb59c414b4573
Yury Luneff <yury@indigosystem.ru>
2013-07-12 00:16:05 +0400

    bumped libmicrohttpd version

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -3,17 +3,17 @@
 # libmicrohttpd
 #
 #############################################################
-LIBMICROHTTPD_VERSION = 0.4.6
+LIBMICROHTTPD_VERSION = 0.9.27
 LIBMICROHTTPD_SOURCE = libmicrohttpd-$(LIBMICROHTTPD_VERSION).tar.gz
 LIBMICROHTTPD_SITE = $(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_INSTALL_STAGING = YES
 
-ifeq ($(BR2_PACKAGE_LIBMICROHTTPD_SSL),y)
-LIBMICROHTTPD_DEPENDENCIES += libgcrypt
-LIBMICROHTTPD_CONF_OPT += --enable-https \
-			  --with-libgcrypt-prefix=$(STAGING_DIR)/usr
-else
+#ifeq ($(BR2_PACKAGE_LIBMICROHTTPD_SSL),y)
+#LIBMICROHTTPD_DEPENDENCIES += libgcrypt
+#LIBMICROHTTPD_CONF_OPT += --enable-https \
+#			  --with-libgcrypt-prefix=$(STAGING_DIR)/usr
+#else
 LIBMICROHTTPD_CONF_OPT += --disable-https
-endif
+#endif
 
 $(eval $(call AUTOTARGETS,package,libmicrohttpd))
